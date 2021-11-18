522cd5bf327cdf6f3867636f3b14e5167f1c09a4
Christian Voigt <1pxsolidblack@gmail.com>
2021-11-18 20:49:34 +0100

    turned declaration generation on, added transformation helpers to index

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,19 @@ export { createSideEffectObservable } from './state/side-effect-observable';
 export { createAccumulationObservable } from './state/accumulation-observable';
 export { RxJsState } from './state/state';
 export { KeyCompareMap, CompareFn, PickSlice } from './core/interfaces';
+export {
+  insert,
+  remove,
+  toDictionary,
+  update,
+  extract,
+  upsert,
+} from './transformation-helpers/array/index';
+export {
+  setProp,
+  patch,
+  deleteProp,
+  dictionaryToArray,
+  toggle,
+  slice,
+} from './transformation-helpers/object/index';
